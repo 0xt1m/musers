398fdc26967e46aeee3d6e4eb379b5efb6e2a427
User <tima.matviiv@gmail.com>
2024-02-03 18:24:06 -0500

    trying to get all the users

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# Check for users, delete, changepasswords
+# Check for users, 
+# delete, 
+# changepasswords
 
 import os
+import pwd, grp
 
+for p in pwd.getpwall():
+    print(p[0], grp.getgrgid(p[3])[0])
 
 def get_users():
 	with open("users.txt", "r") as users_file:
